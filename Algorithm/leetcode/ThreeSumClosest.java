cc10d769fe55221b084f6b5ce5305e004954799e
admin <litengjiang@do-global.com>
2022-04-02 16:33:42 +0800

    保存

---
+++ Algorithm/leetcode/ThreeSumClosest.java
@@ -0,0 +1,24 @@
+package Algorithm.leetcode;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums,int target){
+        Arrays.sort(nums);
+        int res = 0;
+        int n = nums.length;
+        int diff = Integer.MAX_VALUE;
+        for(int i = 0;i<n-2;++i){
+            int t = twoSumClosest(nums, i+1, i-1, target-nums[i]);
+            if(Math.abs(nums[i]+t-target)<diff){
+                res = nums[i] + t;
+                diff = Math.abs(nums[i]+t-target);
+            }
+        }
+        return res;
+    }
+
+    private int twoSumClosest(int[] nums,int start,int end,int target){
+
+    }
+}
