03d1b618ed98ea793f29c3b06b77130f2adcf2ee
litengjiang <1290781598@qq.com>
2024-10-15 15:06:07 +0800

    fix

--- DesignPattern/Adapter/ClassAdapter/Test.java
+++ DesignPattern/Adapter/ClassAdapter/Test.java
@@ -1,17 +1,13 @@
 package DesignPattern.Adapter.ClassAdapter;
 
-import DesignPattern.Adapter.AliPay;
 import DesignPattern.Adapter.PayClient;
-import DesignPattern.Adapter.WeChatPay;
 
 public class Test {
     public static void main(String[] args) {
-        WeChatPay weChatPay = new WeChatPay();
-        PayClient payClient = new WeChatPayAdapter(weChatPay);
+        PayClient payClient = new WeChatPayAdapter();
         payClient.pay(5);
 
-        AliPay aliPay = new AliPay();
-        PayClient aliPayClient = new AliPayAdapter(aliPay);
+        PayClient aliPayClient = new AliPayAdapter();
         aliPayClient.pay(10);
     }
 }
